8f326c5513706bc4c349f4ddd675cc6a113463e7
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2022-02-18 18:48:46 -0500

    Create JawVelHeatmap.m
    Creates a heatmap of jaw velocities on each trial throughout a session.  Can specify which trial types you want to include

---
+++ Plotting Functions/JawVelHeatmap.m
@@ -0,0 +1,20 @@
+function JawVelHeatmap(conditions, jaw, taxis, met)
+toplot = [];
+for c = 1:numel(conditions)
+     toplot = [toplot; met.trialid{c}];  % Trial ID of right, hit, non-early 2AFC trials
+end
+
+trix = sort(toplot);                    % Trial ID of all hit, non-early 2AFC trials
+numtrix = length(trix);              % Num of trials to be plotted
+
+subplot(2,2,2)
+imagesc(taxis(10:end), 1:numtrix,jaw(10:end,trix)')      % Make heatmap of jaw velocity for specified trials
+ax = gca;
+ax.FontSize = 12;
+colorbar
+lim = [0 7];
+caxis(lim)
+xlabel('Time since go-cue (s)','FontSize',13)
+ylabel('Trials','FontSize',13)
+title('Heatmap of jaw velocity','FontSize',14)
+end   % JawVelHeatmap
