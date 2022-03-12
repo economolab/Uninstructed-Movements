6603fc274a05c6e871648320a83eceec70cf8b8a
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2022-03-12 12:12:38 -0500

    Update removeEarlyTrials_PSTH.m
    Save the trial-by-trial PSTHs separately for early move trials and non-early move trials

--- functions/removeEarlyTrials_PSTH.m
+++ functions/removeEarlyTrials_PSTH.m
@@ -4,12 +4,11 @@ for c = 1:numel(obj.condition)      % For every condition...
     % Exclude early movement trials
     if isfield(obj,'earlyMoveix')       
         cond_earlyMove = ismember(trix,obj.earlyMoveix);    % Find trials in this condition that were identified as early move
-        trix = trix(~cond_earlyMove);                       % Remove those trials
-    elseif isfield(obj,'earlyMoveTrial')
-        cond_earlyMove = ismember(trix,obj.earlyMoveTrial);    % Find trials in this condition that were identified as early move
-        trix = trix(~cond_earlyMove); 
+        trid = trix(~cond_earlyMove);                       % Remove those trials
+        earl = trix(cond_earlyMove);
     end
 
-    obj.trialpsth_noEarly{c} = obj.trialpsth(:,:,trix); % Take only the single trial PSTHs from trials without early movements       
+    obj.trialpsth_noEarly{c} = obj.trialpsth(:,:,trid); % Take only the single trial PSTHs from trials without early movements       
+    obj.trialpsth_Early{c} = obj.trialpsth(:,:,earl); % Take only the single trial PSTHs from trials with early movements 
 end
 end
