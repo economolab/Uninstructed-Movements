378f0003b55be4a426d3f17efb46031811031f96
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2022-03-12 12:10:11 -0500

    Improvements on functions
    Made functions for finding probability of jaw movements more generalizable

--- Plotting Functions/plotJawProb_SessAvg.m
+++ Plotting Functions/plotJawProb_SessAvg.m
@@ -1,19 +1,23 @@
 % Plots the probability of jaw movement during a session, separated by
 % trial type
 
-% INPUTS: dirs = Cell array containing the trial type you want to plot (R or
-% L)
-% contexts = Cell array containing the context that you want to plot for
-% ('afc' or 'auw' for 2AFC or Autowater)
+% INPUTS: obj, met for the current session
+% conditions: cell array (1 x num conditions) the behavioral conditions that you want to look at jaw
+% movement for 
+% colors: cell array containing the colors that you want to be used in the
+% plot 
+function plotJawProb_SessAvg(obj,met,conditions,colors)
+edges = met.tmin:met.dt:met.tmax;
 
-function plotJawProb_SessAvg (dirs,contexts,anm,date,colors,edges)
-jawdat = cell(1,numel(dirs));
-for ii = 1:numel(dirs)
-    jawdat{ii} = jawkinSessionAvg(dirs{ii},contexts{ii},anm,date);
-    y = mean(jawdat{ii},2,'omitnan');
-    plot(edges, y(2:end),'color', colors{ii}, 'LineWidth', 2);
+jawprob = jawProbSessionAvg(obj,met,conditions);
+
+for i = 1:numel(jawprob)
+    plot(edges, jawprob{i},'color', colors{i}, 'LineWidth', 2);
     hold on;
 end
+
 xlabel('Time since go-cue (s)','FontSize',13)
 ylabel('Prob of jaw movement','FontSize',13)
 xlim([-2.3 2.5])
+
+end  % end 'plotJawProb_SessAvg'
