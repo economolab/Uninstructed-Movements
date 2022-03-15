d70911e313a1b935316506efcb038aaabcb6416b
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2022-03-15 12:05:58 -0400

    Redundant files

--- Utils/findDataFn.m
+++ Utils/findDataFn.m
@@ -1,11 +1,12 @@
-function objfn = findDataFn(meta)
+function fn = findDataFn(meta)
 contents = dir(meta.datapth);
 contents = {contents.name}';
 
 strToFind = {'data_structure' , meta.anm, meta.date};
 
 [fn,~] = patternMatchCellArray(contents, strToFind, 'all');
-objfn = fn{1};
+if iscell(fn)
+    fn = fn{1};
+end
 
-end % loadRawDataObj
-
+end % findDataFn
--- funcs/oneProbeTrim.m
+++
@@ -1,7 +0,0 @@
-function [obj,params] = oneProbeTrim(obj,params)
-obj.psth = obj.psth{1};
-obj.trialdat = obj.trialdat{1};
-obj.presampleFR = obj.presampleFR{1};
-obj.presampleSigma = obj.presampleSigma{1};
-params.cluid = params.cluid{1};
-end
