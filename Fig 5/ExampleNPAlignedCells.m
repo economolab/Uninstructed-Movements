3d88c07d753d9694c0822bddef7536fc9a3ca5d4
jackieb1 <jackieb1@bu.edu>
2024-02-14 16:58:23 -0500

    Rand

---
+++ Fig 5/ExampleNPAlignedCells.m
@@ -0,0 +1,119 @@
+% Finding CDContext from neural activity that resides within the Null and Potent spaces
+clear,clc,close all
+
+whichcomp = 'LabPC';                                                % LabPC or Laptop
+
+% Base path for code depending on laptop or lab PC
+if strcmp(whichcomp,'LabPC')
+    basepth = 'C:\Code';
+elseif strcmp(whichcomp,'Laptop')
+    basepth = 'C:\Users\Jackie\Documents\Grad School\Economo Lab\Code';
+end
+
+% add paths
+utilspth = [basepth '\Munib Uninstruct Move\uninstructedMovements_v2'];
+addpath(genpath(fullfile(utilspth,'DataLoadingScripts')));
+addpath(genpath(fullfile(utilspth,'funcs')));
+addpath(genpath(fullfile(utilspth,'utils')));
+addpath(genpath(fullfile(utilspth,'figNP')));
+figpth = [basepth  '\Uninstructed-Movements\Fig 3'];
+addpath(genpath(fullfile(figpth,'funcs')));
+addpath(genpath(fullfile(figpth,'Context switching')));
+figpth = [basepth  '\Uninstructed-Movements\Fig 6'];
+addpath(genpath(fullfile(figpth,'funcs')));
+addpath(genpath(fullfile(figpth,'Context_funcs')));
+figpth = [basepth  '\Uninstructed-Movements\Fig 5'];
+addpath(genpath(fullfile(figpth,'funcs')));
+figpth = [basepth  '\Uninstructed-Movements\Fig 2'];
+addpath(genpath(fullfile(figpth,'funcs')));
+
+load([basepth '\Uninstructed-Movements\ContextColormap.mat']);
+%% PARAMETERS
+params.alignEvent          = 'goCue'; % 'jawOnset' 'goCue'  'moveOnset'  'firstLick'  'lastLick'
+
+% time warping only operates on neural data for now.
+% TODO: time warp for video and bpod data
+params.timeWarp            = 0;  % piecewise linear time warping - each lick duration on each trial gets warped to median lick duration for that lick across trials
+params.nLicks              = 20; % number of post go cue licks to calculate median lick duration for and warp individual trials to
+
+params.lowFR               = 1; % remove clusters with firing rates across all trials less than this val
+
+% set conditions to calculate PSTHs for
+params.condition(1)     = {'(hit|miss|no)'};                             % all trials
+params.condition(end+1) = {'hit&~stim.enable&~autowater'};               % all 2AFC hits, no stim
+params.condition(end+1) = {'hit&~stim.enable&autowater'};                % all AW hits, no stim
+params.condition(end+1) = {'miss&~stim.enable&~autowater'};              % error 2AFC, no stim, aw off
+params.condition(end+1) = {'miss&~stim.enable&autowater'};               % error AW, no stim
+
+params.condition(end+1) = {'hit&~stim.enable&~autowater&~early'};               % all 2AFC hits, ~early, no stim
+params.condition(end+1) = {'hit&~stim.enable&autowater&~early'};                % all AW hits, ~early,no stim
+
+
+params.tmin = -3;
+params.tmax = 2.5;
+params.dt = 1/200;
+
+% smooth with causal gaussian kernel
+params.smooth = 15;
+
+% cluster qualities to use
+params.quality = {'all'}; % accepts any cell array of strings - special character 'all' returns clusters of any quality
+
+
+params.traj_features = {{'tongue','left_tongue','right_tongue','jaw','trident','nose'},...
+    {'top_tongue','topleft_tongue','bottom_tongue','bottomleft_tongue','jaw','top_nostril','bottom_nostril'}};
+
+params.feat_varToExplain = 80; % num factors for dim reduction of video features should explain this much variance
+
+params.N_varToExplain = 80; % keep num dims that explains this much variance in neural data (when doing n/p)
+
+params.advance_movement = 0;
+
+params.bctype = 'reflect'; % options are : reflect  zeropad  none
+%% SPECIFY DATA TO LOAD
+
+if strcmp(whichcomp,'LabPC')
+    datapth = 'C:\Users\Jackie Birnbaum\Documents\Data';
+elseif strcmp(whichcomp,'Laptop')
+    datapth = 'C:\Users\Jackie\Documents\Grad School\Economo Lab';
+end
+
+meta = [];
+
+% --- ALM --- 
+meta = loadJEB6_ALMVideo(meta,datapth);     
+meta = loadJEB7_ALMVideo(meta,datapth);              
+meta = loadEKH1_ALMVideo(meta,datapth);     
+meta = loadEKH3_ALMVideo(meta,datapth);
+meta = loadJGR2_ALMVideo(meta,datapth);     
+meta = loadJGR3_ALMVideo(meta,datapth);
+meta = loadJEB13_ALMVideo(meta,datapth);  
+meta = loadJEB14_ALMVideo(meta,datapth);
+meta = loadJEB15_ALMVideo(meta,datapth);
+meta = loadJEB19_ALMVideo(meta,datapth);
+
+params.probe = {meta.probe}; % put probe numbers into params, one entry for element in meta, just so i don't have to change code i've already written
+%% LOAD DATA
+
+% ----------------------------------------------
+% -- Neural Data --
+% obj (struct array) - one entry per session
+% params (struct array) - one entry per session
+% ----------------------------------------------
+[obj,params] = loadSessionData(meta,params);
+%%
+% ------------------------------------------
+% -- Motion Energy --
+% me (struct array) - one entry per session
+% ------------------------------------------
+for sessix = 1:numel(meta)
+    me(sessix) = loadMotionEnergy(obj(sessix), meta(sessix), params(sessix), datapth);
+end
+%% Load kinematic data
+nSessions = numel(meta);
+for sessix = 1:numel(meta)
+    message = strcat('----Getting kinematic data for session',{' '},num2str(sessix), {' '},'out of',{' '},num2str(nSessions),'----');
+    disp(message)
+    kin(sessix) = getKinematics(obj(sessix), me(sessix), params(sessix));
+end
+clearvars -except obj meta kin regr params me
